daeddf0b4b4f9dd48331fc2ad7e5180185e52f55
goodjen <goodjennifer@att.net>
2018-06-06 16:24:34 -0500

    health endpoint

--- src/app.jsx
+++ src/app.jsx
@@ -4,20 +4,23 @@ import history from './history';
 import Header from './components/component-header';
 import PageHome from './routes/home/component-page-home';
 import ScorePage from './routes/scorePage/component-page-scorePage';
+import healthPage from './routes/health/health-page';
 import './app.css';
 
 const App = () => (
   <div>
     <Router history={history}>
       <div className="fullscreen">
-        <Header />
+          <Header />
         <div className="route">
-          <Route exact path="/" component={PageHome} />
+            <Route exact path="/" component={PageHome} />
             <Route exact path="/score" component={ScorePage}/>
+            <Route exact path="/health" component={healthPage}/>
         </div>
       </div>
     </Router>
   </div>
+
 );
 
 export default App;
