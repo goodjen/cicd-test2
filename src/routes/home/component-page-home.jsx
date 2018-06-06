de0d8236a5ade45609cba494448361b07459b786
goodjen <goodjennifer@att.net>
2018-06-06 14:58:33 -0500

    moved comments

--- src/routes/home/component-page-home.jsx
+++ src/routes/home/component-page-home.jsx
@@ -6,10 +6,12 @@ const PageHome = () => (
   <div className="page">
     <div className="page-content column align-center box">
         <h2>Find out how your services have been performing! <hr />
-        Could include a search function to look at the services they are in charge of</h2>
+        </h2>
       <ScoreButton />
     </div>
   </div>
 );
 
 export default PageHome;
+
+//Could include a search function to look at the services they are in charge of
