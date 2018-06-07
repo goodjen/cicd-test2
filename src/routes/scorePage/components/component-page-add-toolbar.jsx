600aa8538e18b3e0fe5ddeea829590192faf7f46
goodjen <goodjennifer@att.net>
2018-06-07 15:39:36 -0500

    158198497 deleted unused code and references to it

--- src/routes/scorePage/components/component-page-add-toolbar.jsx
+++ src/routes/scorePage/components/component-page-add-toolbar.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Toolbar from '../../../components/component-toolbar';
 import history from '../../../history';
 
 export default () => <Toolbar onClickBack={() => { history.goBack(); }} />;
