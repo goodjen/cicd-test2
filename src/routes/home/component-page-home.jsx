1130b4bba82de7f531455a65390afb08b987ad76
goodjen <goodjennifer@att.net>
2018-06-08 08:50:53 -0500

    no merge error?

--- src/routes/home/component-page-home.jsx
+++ src/routes/home/component-page-home.jsx
@@ -3,13 +3,15 @@ import ScoreButton from './components/component-button-scores';
 import Toolbar from '../../components/component-toolbar';
 
 const PageHome = () => (
-  <div className="page">
-    <div className="page-content column align-center box">
-        <h2>Find out how your services have been performing! <hr />
-         dev mode</h2>
-      <ScoreButton />
+    <div className="page">
+        <div className="page-content column align-center box">
+            <h2>Find out how your services have been performing! <hr />
+            </h2>
+            <ScoreButton />
+        </div>
     </div>
-  </div>
 );
 
 export default PageHome;
+
+//Could include a search function to look at the services they are in charge of
