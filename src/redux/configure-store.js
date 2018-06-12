13ec863ea7442aab9eda06d79c879122b90b16c7
Nick Bunn <nick_bunn@homedepot.com>
2018-06-12 10:45:09 -0500

    added redux-thunk

--- src/redux/configure-store.js
+++ src/redux/configure-store.js
@@ -1,10 +1,11 @@
-import { compose, createStore } from 'redux';
+import { compose, createStore, applyMiddleware } from 'redux';
 import rootReducer from './root-reducer';
+import thunk from 'redux-thunk';
 // eslint-disable-next-line no-underscore-dangle
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function configureStore(initialState) {
-  const store = createStore(rootReducer, initialState, composeEnhancers());
+  const store = createStore(rootReducer, applyMiddleware(thunk), initialState, composeEnhancers());
   if (module.hot) { // This part enables Webpack to hot reload reducers
     module.hot.accept('./root-reducer', () => {
       // eslint-disable-next-line global-require
