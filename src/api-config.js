32aafacfc7876d5a3427aa22a9e3fee95db61f4f
goodjen <goodjennifer@att.net>
2018-06-18 11:52:04 -0500

    better

--- src/api-config.js
+++ src/api-config.js
@@ -2,11 +2,11 @@ const apiVersion = 'v1';
 
 export default function apiConfig(){
     var API_ROOT='';
-    if (!process.env.Node_ENV || process.env.NODE_ENV === 'development') {
-        API_ROOT = `http://localhost.homedepot.com/api/${apiVersion}`;
+    if (process.env.NODE_ENV === 'production') {
+        API_ROOT = `http://api.revaluation.hd-opstool-np.gcp.homedepot.com/api/${apiVersion}`;
     }
     else {
-        API_ROOT = `http://api.revaluation.hd-opstool-np.gcp.homedepot.com/api/${apiVersion}`;
+        API_ROOT = `http://localhost.homedepot.com/api/${apiVersion}`;
     }
     return API_ROOT;
 }
