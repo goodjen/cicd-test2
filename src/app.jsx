22352abdd225462bca41d7be49603350fb7921ab
goodjen <goodjennifer@att.net>
2018-06-18 16:20:30 -0500

    navbar functional, stylish, and made old tool obsolete

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,6 @@ import { Router, Route } from 'react-router-dom';
 import history from './history';
 import Header from './components/component-header';
 import PageHome from './routes/home/component-page-home';
-import ScorePage from './routes/scorePage/component-page-scorePage';
 import healthPage from './routes/health/health-page';
 import appPage from './routes/applications/app-page';
 import sresPage from './routes/sres/sres-page';
@@ -16,7 +15,6 @@ const App = () => (
               <Header />
         <div className="route">
             <Route exact path="/" component={PageHome} />
-            <Route exact path="/score" component={ScorePage}/>
             <Route exact path="/health" component={healthPage}/>
             <Route exact path="/applications" component={appPage}/>
             <Route exact path="/sres" component={sresPage}/>
--- src/components/component-header.jsx
+++ src/components/component-header.jsx
@@ -5,10 +5,13 @@ import '../app.css';
 const Header = () => (
   <div className="topnav">
       <img alt="" src={Logo} className="app-logo" />
-      <h4>REvaluate</h4>
-        <a class="active" href="/">Home</a>
-         <a href="applications">Applications</a>
-         <a href="sres">Sres</a>
+      <h4><a className="active topnav-title" href="/">REvaluate</a></h4>
+      <div className="push">
+          <h3><a href="applications">Applications</a></h3>
+      </div>
+      <div className="from-end">
+      <h3><a href="sres">Sres</a></h3>
+      </div>
   </div>
 
 );
